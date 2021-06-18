899b6fad59f3f30d8ed07d184b66b44026a98aea
#priyu@123 <priyaengg26@gmail.com>
2021-06-18 23:10:03 +0700

    final

---
+++ quiz/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "‘cin’ is an __",
+    answer: "object",
+    options: [
+      "class",
+      "object",
+      "package",
+      "namespace"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Escape sequence character '\0' occupies __ amount of memory.",
+    answer: "1",
+    options: [
+      "0",
+      "1",
+      "2",
+      "4"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which feature of the OOPS gives the concept of reusability?",
+    answer: "Inheritance",
+    options: [
+      "Abstraction",
+      "Encapsulation",
+      "Inheritance",
+      "None Of The Obove"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which type of data file is analogous to an audio cassette tape?",
+    answer: "Sequential access file",
+    options: [
+      "Random access file",
+      "Sequential access file",
+      "Binary file",
+      "Source code file"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does a floating point number contain?",
+    answer: "Both integer and fractional part.",
+    options: [
+      "none",
+      "Both integer and fractional part.",
+      "Fractional part only.",
+      "Integer part only."
+    ]
+  },
+ 
+];
